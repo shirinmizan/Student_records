ae4e0149722db65c7030d26046d22b7c0c24f7b0
Shirin Mizan <shirinmizan@gmail.com>
2016-11-17 17:12:50 -0500

    Update HomeController.java

--- src/ca/sheridancollege/controllers/HomeController.java
+++ src/ca/sheridancollege/controllers/HomeController.java
@@ -1,13 +1,11 @@
 package ca.sheridancollege.controllers;
 
 import java.util.Date;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import ca.sheridancollege.beans.Student;
 
 @Controller
